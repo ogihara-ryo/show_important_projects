10dcc394b6325818e63c4dfdf321d2f7f7904431
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-12-03 18:43:46 +0900

    ProjectsHelper#render_project_hierarchy の拡張

---
+++ lib/show_important_projects/projects_helper_patch.rb
@@ -0,0 +1,36 @@
+require_dependency 'projects_helper'
+
+module ShowImportantProjects
+  module ProjectsHelperPatch
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method_chain :render_project_hierarchy, :show_important_projects
+    end
+
+    def render_project_hierarchy_with_show_important_projects(projects)
+      s = render_important_projects
+      s << render_project_hierarchy_without_show_important_projects(projects)
+      s.html_safe
+    end
+
+    private
+
+    def render_important_projects
+      s = "<h2>重要プロジェクト</h3><ul id='important_projects' class='projects root'>"
+      Project.importants.each do |project|
+        s << "<li id='favorite_#{project.id}' class='root'><div class='root'>"
+        s << link_to_project(project, {}, class: "#{project.css_classes} #{User.current.member_of?(project) ? 'my-project' : nil}")
+        s << '</div></li>'
+      end
+      s << '</ul>'
+      s << "<hr style='margin: 24px 0;' />"
+    end
+  end
+end
+
+ActionDispatch::Reloader.to_prepare do
+  unless ProjectsHelper.included_modules.include?(ShowImportantProjects::ProjectsHelperPatch)
+    ProjectsHelper.send(:include, ShowImportantProjects::ProjectsHelperPatch)
+  end
+end
